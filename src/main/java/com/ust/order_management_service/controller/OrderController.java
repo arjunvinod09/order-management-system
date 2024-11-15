28ee5cd575f6dd2b254d3cc033d2d09cbc89b874
arjun.vinod <arjunvinod09@gmail.com>
2024-11-15 11:41:21 +0530

    unused import

--- src/main/java/com/ust/order_management_service/controller/OrderController.java
+++ src/main/java/com/ust/order_management_service/controller/OrderController.java
@@ -6,21 +6,17 @@ import com.ust.order_management_service.dto.UserDTO;
 import com.ust.order_management_service.exception.OrderNotFoundException;
 import com.ust.order_management_service.exception.UserNotFoundException;
 import com.ust.order_management_service.model.Order;
-import com.ust.order_management_service.model.User;
 import com.ust.order_management_service.service.OrderServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1")
